162ea20cf9daec29ecd9ac0d32e5c55514cfa94e
mac <shattyk7979@mail.ru>
2022-01-06 11:28:05 +0600

    cod change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("guygydguyeg");
-        System.out.println("guygydguyeg");
-        System.out.println("guygydguyeg");
+        System.out.println("hello world");
+        System.out.println("hello git");
+        System.out.println("hello");
     }
 }
